ff6562ef9fe8994c382e643c6045e69cf63c57e8
aur <0619203910@uma.es>
2017-03-16 16:32:28 +0100

    modificaciones Aurel

--- Lib.java
+++ Lib.java
@@ -7,13 +7,17 @@ public class Lib {
 
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+	
     }
 
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	System.out.println("modificado por Aurel Orjan"); // Aurel 
     }
 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones Aurel
+	System.out.println("Aurel inserta esta sentencia");
     }
 }
